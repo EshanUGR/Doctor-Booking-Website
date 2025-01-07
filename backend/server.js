207974e4a0d982e798059ea661cc53ab9c86d718
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2025-01-07 09:27:14 +0530

    create server file

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import conectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js';
+//app config
+
+const app=express();
+const port=process.env.PORT || 4000;
+conectDB();
+connectCloudinary();
+app.use(express.json());
+app.use(cors());
+//api endpoint
+
+app.use('/api/admin',adminRouter)
+app.get('/',(req,res)=>
+{
+  res.send('API IS WORKING');
+})
+app.listen(port,()=>
+{
+  console.log( `server is running on port : ${port}`);
+
+})
