f6ab71fbf1df29df90fb1ed83cb8d965611730c6
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2025-01-15 11:37:21 +0530

    created context files

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
 
-const AppContext = () => {
+import { createContext } from 'react'
+
+export const AppContext=createContext()
+
+const AppContextProvider=(props)=>
+{
+  const value={
+
+
+  }
+
   return (
     <div>
-      
+
+      <AppContext.Provider value={value}>
+{props.children}
+
+        
+      </AppContext.Provider>
     </div>
   )
 }
 
-export default AppContext
+export default AppContextProvider;
