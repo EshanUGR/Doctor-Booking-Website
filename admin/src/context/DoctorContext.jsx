a47729c076b1e0b97dcfeb344a8328b4c26fa981
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2025-01-15 11:36:34 +0530

    completed doctor controll

---
+++ admin/src/context/DoctorContext.jsx
@@ -0,0 +1,25 @@
+
+import { createContext } from 'react'
+
+export const DoctorContext=createContext()
+
+const DoctorContextProvider=(props)=>
+{
+  const value={
+
+
+  }
+
+  return (
+    <div>
+
+      <DoctorContext.Provider value={value}>
+{props.children}
+
+        
+      </DoctorContext.Provider>
+    </div>
+  )
+}
+
+export default DoctorContextProvider;
