742698922b39bb12712360234af85a655e1858c4
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2025-01-15 11:38:45 +0530

    change app js file

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import Login from './pages/Login'
 
-function App() {
- 
-  return (
-<div>
-<Login/>
+import {ToastContainer,toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-</div>
-  )
+import {AppContext} from './context/AppContext.jsx'
+import { AdminContext } from './context/AdminContext'
+import Navbar from './components/Navbar.jsx'
+function App() {
+
+  const {aToken}=useContext(AdminContext);
+ 
+  return aToken ? (
+    <div className="bg-[#F8F9FD]">
+      <ToastContainer />
+
+      <Navbar />
+    </div>
+  ) : (
+    <>
+   
+      <Login />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default App
